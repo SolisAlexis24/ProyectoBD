da1f618d9557f530b6cb674a6947709db08fe77a
SolisAlexis24 <ianalexis9137@gmail.com>
2024-06-02 20:27:01 -0600

    Modificacion al rol de admin para crear sinonimos publicos

--- s-01-usuarios.sql
+++ s-01-usuarios.sql
@@ -29,7 +29,7 @@ create user sc_proy_invitado identified by guest;
 
 --Creacion de roles
 create role rol_admin;
-grant create session, create table, create sequence to rol_admin;
+grant create session, create table, create sequence, create public synonym to rol_admin;
 create role rol_invitado;
 grant create session to rol_invitado;
 
