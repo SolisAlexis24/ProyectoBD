e055906a651aef67c3cf1d682065e2e3a55adc57
JairCR09 <90934752+JairCR09@users.noreply.github.com>
2024-06-04 17:17:52 -0600

    Update s-08-vistas.sql

--- s-08-vistas.sql
+++ s-08-vistas.sql
@@ -14,13 +14,13 @@ from (select v.vuelo_id, count(*) num_paquetes from vuelo v, paquete p where v.v
 (select vuelo_id, num_vuelo, fecha_salida, fecha_aprox_llegada from vuelo where es_comercial=0 and es_carga=1 and to_char(fecha_salida,'dd-mm-yyyy')=to_char(sysdate,'dd-mm-yyyy')) r
 where q.vuelo_id=r.vuelo_id;
 
-create or replace view empleados_jefes(empleado_id, nombre, apellidos, rfc, puesto)
+create or replace view v_empleados_jefes(empleado_id, nombre, apellidos, rfc, puesto)
 as select r.empleado_id, r.nombre, r.apellidos, r.rfc, r.puesto
 from (select jefe_id from empleado where jefe_id is not null) q,
 (select e.empleado_id, e.nombre, e.apellidos, e.rfc, p.nombre puesto from empleado e, puesto p where e.puesto_id=p.puesto_id) r
 where r.empleado_id=q.jefe_id;
 
-create or replace view vuelos_cancelados(num_vuelo, comercial, carga, fecha_salida)
+create or replace view v_vuelos_cancelados(num_vuelo, comercial, carga, fecha_salida)
 as select v.num_vuelo, v.es_comercial, v.es_carga, v.fecha_salida
 from vuelo v, vuelo_estado ve 
 where v.vuelo_estado_id=ve.vuelo_estado_id and ve.clave='CANCELADO';
