430adea19f5838090e010b1c26ad4faf4cba25d5
SolisAlexis24 <ianalexis9137@gmail.com>
2024-06-04 16:38:28 -0600

    Corrección en nombre de archivos

--- s-00-main.sql
+++ s-00-main.sql
@@ -1,7 +1,7 @@
 @s-01-usuarios.sql
 @s-02-entidades.sql
 @s-03-tablas-temporales.sql
-@s-04-tablas-temporales.sql
+@s-04-tablas-externas.sql
 @s-05-secuencias.sql
 @s-06-indices.sql
 @s-07-sinonimos.sql
