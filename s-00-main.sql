375f153c61cf8db4e7c462f29a378aeb725ceed3
SolisAlexis24 <ianalexis9137@gmail.com>
2024-06-04 16:31:43 -0600

    Cambio para invocar archivos

--- s-00-main.sql
+++ s-00-main.sql
@@ -1,9 +1,9 @@
 @s-01-usuarios.sql
 @s-02-entidades.sql
 @s-03-tablas-temporales.sql
-
+@s-04-tablas-temporales.sql
 @s-05-secuencias.sql
-
+@s-06-indices.sql
 @s-07-sinonimos.sql
 
 @s-09-carga-inicial.sql
