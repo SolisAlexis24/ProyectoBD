6584ff43046356221f6e2e4977340037fe0eaa6c
JairCR09 <90934752+JairCR09@users.noreply.github.com>
2024-06-05 13:05:42 -0600

    Add files via upload

---
+++ s-16-fx-total-sueldo-prueba.sql
@@ -0,0 +1,19 @@
+--@Autor(es): Alexis S., Jair C.
+--@Fecha creación: 02/06/2024
+--@Descripción: Script prueba funcion sueldo por departamento del proyecto
+
+Prompt =======================Prueba 1==========================
+
+set serveroutput on
+
+declare
+
+v_sueldo_total number(8,2);
+
+begin
+  select sueldo_total('MANTENIMIENTO') as "sueldo_total" into v_sueldo_total from dual;
+  if v_sueldo_total is null then
+    dbms_output.put_line('No se encontró el puesto');
+  end if;
+end;
+/
